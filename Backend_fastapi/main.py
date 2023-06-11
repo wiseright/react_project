355c6a75df70debf815bba0b3c997a8fd2f0ad3d
wiseright <allencavaliereoscuro@gmail.com>
2023-06-11 18:04:59 +0200

    added backend with FastAPI

---
+++ Backend_fastapi/main.py
@@ -0,0 +1,64 @@
+from typing import Union
+import json
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+import uvicorn
+from random import random
+from fastapi.middleware.cors import CORSMiddleware
+
+FILENAME = 'posts.json'
+
+class Posts(BaseModel):
+    author: str
+    body: str
+
+# To get the content of post file
+def get_posts(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
+
+# To append the content of file
+def store_posts(filename, data):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(data, indent=4))
+
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8080",
+]
+
+# To manage the CORS
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/posts")
+def read_root():
+    data = get_posts('posts.json')
+    return {"Posts": data['posts']}
+
+
+@app.get("/posts/{post_id}")
+def read_item(post_id: int):
+    return {"item_id": post_id}
+
+@app.post("/posts")
+async def create_item(post: Posts):
+    print(type(post))
+    data = get_posts(FILENAME)
+    print(data)
+    diz = post.dict()
+    diz['id'] = str(random())
+    data['posts'].append(diz)
+    store_posts(FILENAME, data)
+    return post
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8080)
--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 4. Enter into Backend_node folder and digit:
 	* npm install
 	* npm start
+5. [ALTERNATIVELY] Enter into Backend_fastapi folder and digit:
+	* uvicorn main:app  --reload --host 0.0.0.0 --port 8080
+	* If you need to debug Python script, uncomment the last to row in the script
