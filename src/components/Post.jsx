31f95574558b41e6f4f855eb0f993fff971efe25
wiseright <allencavaliereoscuro@gmail.com>
2023-06-11 11:34:42 +0200

    dinamically add the posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 import classes from './Post.module.css';
 
-function Post(props){   
+function Post({author, body}){   
 
     return (
     <div className={classes.post}>
-        <p className={classes.author}>{props.author}</p>
-        <p className={classes.text}>{props.content}</p>
+        <p className={classes.author}>{author}</p>
+        <p className={classes.text}>{body}</p>
     </div>);
 }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,47 +2,35 @@ import classes from './PostsList.module.css';
 import Post from './Post';
 import NewPost  from './NewPost';
 import Modal from './Modal';
-import { useState } from 'react';
+import  { useState } from 'react';
 
 
 function PostList({isPosting, onStopPosting}){
-    // The states:
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
+    const [posts, setPosts] = useState([]);
 
+    function addPostsHandler(postsData){
+        setPosts((existingPosts) => [postsData, ...existingPosts]);
+    }
     
-    // The function Handler:
-    function bodyChangeHandler(event){
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event){
-        setEnteredAuthor(event.target.value);
-    }
-
-
-
-    // The variables:
-    let modalContent;
-
-    if (isPosting){
-        modalContent = (
-            <Modal onClose={onStopPosting}>
-                <NewPost 
-                    onBodyChange={bodyChangeHandler} 
-                    onAuthorChange={authorChangeHandler} 
-                />
-            </Modal>
-        );
-    }
-
     return(
         <>
-            {modalContent}
-            <ul className={classes.posts}>
-                <Post author={enteredAuthor} content={enteredBody} />
-                <Post author="the worst" content="this is another test"/>
-            </ul>
+            {isPosting &&
+                <Modal onClose={onStopPosting}>
+                    <NewPost onCancel={onStopPosting} onAddPost={addPostsHandler} />
+                </Modal>
+            }
+            {posts.length > 0 &&
+                <ul className={classes.posts}>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                </ul>
+            }
+            {posts.length === 0 &&
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <h2>There are no posts yet!</h2>
+                    <p>Starting add someone...</p>
+                </div>
+
+            }
         </>
     );
 }
